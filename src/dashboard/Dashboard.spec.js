8c57c3931c38f50d7f7879c7557f63c050a7566c
Phinehas <kohler.nai@gmail.com>
2019-02-05 20:15:43 -0700

    Wrote tests to make sure Dashboard is rendering correctly, and that it's showing contols and display components

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
-// Test away
+// Test away!
+import Display from './Dashboard';
+import Controls from './Dashboard';
+import { render } from 'react-testing-library';
+import React from 'react';
+import Dashboard from './Dashboard';
+
+describe('The Dashboard component', () => {
+    it('renders without crashing', () => {
+        render(<Dashboard />)
+    })
+    it('renders the Display component', () => {
+        render(<Display />)
+    })
+    it('renders the Controls component', () => {
+        render(<Controls />)
+    })
+})
